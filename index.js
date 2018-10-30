028f0f446a05a75eb39dcf52a7dd462bffbc9ae7
Alexandra Johnson <alexandra@queryclick.com>
2018-10-30 20:08:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ document.addEventListener('keydown', function(e) {
 //right
 
 function moveDodgerRight() {
-  var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(leftNumbers, 10)
  
   if (right > 0) {
     dodger.style.right = `${right - 1}px`
