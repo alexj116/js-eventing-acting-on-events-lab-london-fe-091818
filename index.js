441f3a6c6d78c57df1a13e2644a14b24434e0fe9
Alexandra Johnson <alexandra@queryclick.com>
2018-10-30 20:11:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,21 +18,3 @@ document.addEventListener('keydown', function(e) {
   }
 })
 
-//right
-
-function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(leftNumbers, 10)
- 
-  if (right > 0) {
-    dodger.style.right = `${right - 1}px`
-  }
-}
-
-//event listener right
-
-document.addEventListener('keydown', function(e) {
-  if (e.which === 39) {
-    moveDodgerRight()
-  }
-})
